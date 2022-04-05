58eae3cb1350601af1b640030b3557850ae87457
binod164 <binod7_grg@hotmail.com>
2022-04-05 16:42:51 -0500

    Complete all questions

--- python-functions.py
+++ python-functions.py
@@ -6,9 +6,6 @@ def sum_to(n):
     sum += num
   return sum
 
-print(sum_to(6))
-
-
 #2 Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
 
 def largest(nums):
@@ -18,16 +15,10 @@ def largest(nums):
       largest_num = num
   return largest_num
 
-print(largest([10, 4, 2, 231, 91, 54]))
-
-
 #3 Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
 
 def occurances(string, substr):
   return string.count(substr)
-
-print(occurances('fleep floop', 'e'))
-
 
 #4 Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product. HINT: Review your notes on args.
 
@@ -36,5 +27,3 @@ def product(*args):
   for arg in args:
     result *= arg
   return result
-
-print(product(4, 0.5, 5))
